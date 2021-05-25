6acd77a23c9f04b350c2368b26a6154e51ab6478
N4t4nis <n4t4nistorus@gmail.com>
2021-05-25 15:28:40 +0200

    amélioration du brut-force

--- bruteforce.py
+++ bruteforce.py
@@ -1,46 +1,44 @@
+import itertools
 from pprint import pprint
 import time
 import json
 
 START_TIME = time.time()
-
-
 with open('actions_algoinvest.json') as f:
     data = json.load(f)
-
 
 # Chaque action ne peut être achetée qu'une seule fois.
 # Nous ne pouvons pas acheter une fraction d'action.
 # Nous pouvons dépenser au maximum 500 euros par client.
 
 
-# for action in actions_algoinvest:
-#     print(action["benef%_apres2ans"])
+"""
+POUR  TESTER TOUTES LES COMBINAISONS POSSIBLE (bruteforce):
+pour chaque nombre, tester tout les nombre en deuxieme, puis en toisieme... 
+jusqua 500 de somme max
+
+"""
+liste_nombre = [20, 30, 50]
+all_combinations = [] #liste contenant toutes les listes de combinaisons // liste de liste
 
 
-def get_top_investissement(list_actions, MISE_MAX_PAR_CLIENT=500, porte_monnaie=0):
-    """
-    :param list_actions: liste d actions(boursière) à trier
-    :param MISE_MAX_PAR_CLIENT: par défaut le client investis 500 euros
-    :param porte_monnaie: par défaut le porte monnaie du client commence à 0 euros
-    """
-    top_actions = sorted(list_actions, key=lambda tri: tri.get("profit"), reverse=True)
-    pprint(top_actions)
-    investissement = []
-    total_return = 0
-    for action in top_actions:
-        if action["price"] + porte_monnaie > MISE_MAX_PAR_CLIENT:
-            continue
-        investissement.append(action)
-        porte_monnaie += action["price"]
-        benef = (action["price"] * action["profit"]) / 100
-        total_return += benef
-        print(benef)
-    print(porte_monnaie)
-    print(total_return)
-    return investissement
+all_price = [action["price"] for action in data["les_20_actions"]]
+print(all_price)
+
+for L in range(len(all_price)+1):
+    for subset in itertools.combinations(all_price, L):
+        if sum(subset) <= 500:
+            all_combinations.append(subset)
+
+# pprint(all_combinations)
+print(len(all_combinations))
+
+for numb, combi in enumerate(all_combinations):
+    print(f"combinaison n°{numb}")
+    print(f"côuts total :{sum(combi)}")
 
 
-pprint(get_top_investissement(data["les_20_actions"]))
+
+
 
 print("--- %s seconds ---" % (time.time() - START_TIME))
