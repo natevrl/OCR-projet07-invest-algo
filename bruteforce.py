cb4ba07ea02d7c6fdde3ae832e741d2943cea602
N4t4nis <n4t4nistorus@gmail.com>
2021-05-28 14:32:24 +0200

    projet 07 finit à 75% (faire les powerpoint, analyse de performance et efficacité de l'algo (notation big-O))

--- bruteforce.py
+++ bruteforce.py
@@ -25,15 +25,24 @@ dict_de_tests = [{"nom": "Action-1", "price": 240, "profit": 5},
 
 
 def create_all_combinations(fichier_json, liste_des_prix, mise_max_du_client=500):
+    """
+    Cette fonction créer tout les combinaison possible d'une liste de nombres
+    :param fichier_json: correspond a un dictionnaire contenant des actions
+    :param liste_des_prix: liste des prix uniquement de toutes les actions d'un dictionnaire
+    :param mise_max_du_client: la mise que le client va investir, par défaut 500 pour ce projet
+    :return: Retourne une liste de listes contenant TOUTES les combinaison d'actions possible (plus d'1 millions)
+    """
     all_combinations = []  # liste contenant toutes les listes de combinaisons // liste de tuples
     for L in range(len(liste_des_prix)):
         # subset sera forcement des nombres dans une tuple
         for subset in itertools.combinations(liste_des_prix, L):
             combi = []
+            # je transforme les tuples de nombres en liste de dictionnaire contenant toutes les informations des actions
             if sum(subset) <= mise_max_du_client:
                 for dicts in fichier_json:
                     if dicts["price"] in subset:
                         combi.append(dicts)
+            # si la liste n'est pas vide:
             if combi:
                 all_combinations.append(combi)
     return all_combinations
@@ -41,33 +50,43 @@ def create_all_combinations(fichier_json, liste_des_prix, mise_max_du_client=500
 
 # def find_best_combinations (best profits + near 500€)
 def find_best_combination(liste):
+    """
+    Cette fonction ce couple avec la fonction 'create_all_combinations', elle renvoie l'action la plus rentable/
+    parmis toutes les combinaisons et l'affiche
+    :param liste: la liste de toutes les combinaisons
+    :return:
+    """
     best_profits = 0
     best_action = str()
     for numb, combi in enumerate(liste, 1):
         prix_total = sum(action["price"] for action in combi)
-        print(f"combinaison n°{numb} - côuts total: {prix_total}")
+        # print(f"combinaison n°{numb} - côuts total: {prix_total}")
         #profits de la combi en euro
         benef_total = 0
         for action in combi:
             benef_par_action = (action["price"] * action["profit"]) / 100
-            print(benef_par_action)
+            # print(benef_par_action)
             benef_total += benef_par_action
         rendement = (benef_total / prix_total) * 100
-        print(f"total : {round(benef_total, 2)} ({round(rendement, 2)}%)")
+        # print(f"total : {round(benef_total, 2)} ({round(rendement, 2)}%)")
         if benef_total > best_profits:
             best_profits = benef_total
             best_action = combi
+    return best_action
 
-    print(best_action)
-    prix_total = sum(action["price"] for action in best_action)
-    benef_total = sum(action["price"] * action["profit"] / 100 for action in best_action)
+def affichage(actions_a_afficher):
+    print(actions_a_afficher)
+    prix_total = sum(action["price"] for action in actions_a_afficher)
+    benef_total = sum(action["price"] * action["profit"] / 100 for action in actions_a_afficher)
     rendement = (benef_total / prix_total) * 100
     print(f"côuts total: {prix_total}€, profits: {round(benef_total, 2)}€, rendement: {round(rendement, 2)}%")
-    print(f"\nCombinaisons : {len(liste)}")
+
 
 
 if __name__ == "__main__":
     init_combi = create_all_combinations(liste_actions, all_price)
-    find_best_combination(init_combi)
+    best_combi = find_best_combination(init_combi)
+    affichage(best_combi)
+    print(f"\nCombinaisons : {len(init_combi)}")
     print("--- %s seconds ---" % (time.time() - START_TIME))
 
--- optimized.py
+++ optimized.py
@@ -7,6 +7,15 @@ START_TIME = time.time()
 with open('actions_algoinvest.json') as f:
     data = json.load(f)
 
+"""
+Cet algorithme commence par trier la liste d'actions par ordre décroissant de profit
+du plus rentable au moins rentable.
+Il ajoute dans une nouvelle list les actions en partant du haut jusqu'a atteindre la mise du client
+(500 euros pour ce projet).
+ Option 01 : Si une actions dépasse les 500 eu, il passe a l'actions d'en dessous.
+ Option 02 : Dès que tu ajoutes une action qui fait dépasser la mise client, tu l'enlève de la liste et tu arrete l'algo
+"""
+
 
 def get_top_investissement(list_actions, mise_max_par_client=500, porte_monnaie=0):
     """
@@ -20,8 +29,10 @@ def get_top_investissement(list_actions, mise_max_par_client=500, porte_monnaie=
     total_return = 0
     moyenne_profits = 0
     for action in top_actions:
+        # Si l'action fait entre 1 et 500€
         if 0 < action["price"] <= mise_max_par_client:
-            # option 1 :
+            # option 1 : Si l'action + la somme du porte monnai actuel dépasse la mise client,
+            # Alors tu passe a l'action suivante (continue)
             # if action["price"] + porte_monnaie > mise_max_par_client:
             #     continue
             investissement.append(action)
@@ -29,7 +40,7 @@ def get_top_investissement(list_actions, mise_max_par_client=500, porte_monnaie=
             benef = (action["price"] * action["profit"]) / 100
             moyenne_profits += action["profit"]
             total_return += benef
-            # option 2
+            # option 2 : Dès que tu ajoute une action qui fait dépasser la mise client, tu l'enlève et tu arrete l'algo
             if porte_monnaie > mise_max_par_client:
                 investissement.pop(-1)
                 porte_monnaie -= action["price"]
@@ -38,19 +49,17 @@ def get_top_investissement(list_actions, mise_max_par_client=500, porte_monnaie=
             print(benef)
     # print(f"total cost : {round(porte_monnaie, 2)}")
     # print(f"total return : {round(total_return, 2)}")
-    return investissement, round(porte_monnaie, 2), round(total_return, 2), round(total_return/porte_monnaie*100, 2)
+    return investissement, round(porte_monnaie, 2), round(total_return, 2), round(total_return / porte_monnaie * 100, 2)
 
 
-test = get_top_investissement(data["les_20_actions"])
-print("\nSelon l'algo, voici la meilleurs combinaison d'action : ")
-pprint(test[0])
-print(f" total cost : {test[1]}")
-print(f" total return : {test[2]}")
-print(f" profits : {test[3]}%")
+if __name__ == "__main__":
+    test = get_top_investissement(data["Sienna_dataset1"])
+    print("\nSelon l'algo, voici la meilleur combinaison d'action : ")
+    # la fonction return une tuple de plusieurs value,
+    # j'appelle l'instance de la fonction avec l'index de chaque valeurs
+    pprint(test[0])
+    print(f" total cost : {test[1]}")
+    print(f" total return : {test[2]}")
+    print(f" profits : {test[3]}%")
 
-
-# for numb, action in  enumerate(get_top_investissement(data["Sienna_dataset1"])):
-#     print(numb+1, action['name'])
-
-
-print("--- %s seconds ---" % (time.time() - START_TIME))
+    print("--- %s seconds ---" % (time.time() - START_TIME))
